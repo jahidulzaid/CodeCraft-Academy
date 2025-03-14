bb473d6864010db7036cf0fb9309e04bd7bb3a1e
Jahidul Islam <jahidulzaid@gmail.com>
2025-03-14 21:55:02 +0600

    laravel project init-ed with the view index and assets

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebsiteController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [WebsiteController::class, 'index']);
+
+
+
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
